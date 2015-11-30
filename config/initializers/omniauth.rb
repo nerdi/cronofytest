8d9f78068a8b582f92bd2fd5436579d25223061e
Dan Solo <dan@solocreative.com.au>
2015-11-30 20:59:50 +1100

    MORE TIME WASTING

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :cronofy, ENV["CRONOFY_CLIENT_ID"], ENV["CRONOFY_CLIENT_SECRET"], {
     scope: "read_account list_calendars read_events create_event delete_event"
-    redirect_uri: ""
+    redirect_uri: "https://heroku.com/nerdi/cronofytest"
   }
 
 end
